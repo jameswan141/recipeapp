c88d8a70d3f3ff7eb998056790d505afebb61a24
jameswan141 <70609875+jameswan141@users.noreply.github.com>
2021-09-23 10:19:48 -0400

    commit on "main"

--- food_app/src/App.js
+++ food_app/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
+  const [query, setQuery] = useState("weeknight");
   useEffect(() => {
     getRecipes();
   }, [query])
